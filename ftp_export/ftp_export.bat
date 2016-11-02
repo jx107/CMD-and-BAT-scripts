b3d1927edb4f4008043e77b61f2293392f537396
Vitaly <zurgllz@ya.ru>
2016-11-02 06:04:11 +0300

    test desc

---
+++ ftp_export/ftp_export.bat
@@ -0,0 +1,6 @@
+:: don't name your file ftp.bat, script will run itself
+ftp -v -n -s:settings.txt               :: we run FTP and open settings.txt
+cls                                     :: clear screen
+@echo Файл text.txt отправлен
+@echo Это окно можно закрыть
+pause                                   :: wait when user read text and close window
